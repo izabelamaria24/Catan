9580a02c288793f766217c700917406055a96e85
Jilavu Izabela <izabelajilavu@gmail.com>
2024-04-04 23:17:40 +0300

    Entity subclasses header files added

---
+++ headers/Player.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <string>
+#include "Entity.h"
+
+class Player : Entity {
+    std::string name;
+};
---
+++ headers/ResourceBank.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "Entity.h"
+
+class ResourceBank : Entity {
+public:
+    ResourceBank() = default;
+//    void distributeResources(); // TODO
+//    void trade(Entity& entity); // TODO
+};
